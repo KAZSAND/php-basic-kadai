be1857bc9675c43538a19e00d1efac41a2b63a69
KAZSAND <kaz_410_nc@hotmail.com>
2025-06-02 17:07:35 +0900

    Add files via upload
    1st attempt

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kadai_007</title>
+</head>
+<body>
+    <P>
+        <?php
+        $product_details=["name"=>"onion","price"=>"200","weight"=>"160"];
+
+        print_r($product_details);
+        
+        
+        ?>
+    </P>
+    
+</body>
+</html>
