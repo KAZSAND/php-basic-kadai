f7c617151d7264c5ac1a89db8518da0709ac9b43
KAZSAND <kaz_410_nc@hotmail.com>
2025-06-07 06:49:24 +0900

    Add files via upload
    3rd

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -10,7 +10,7 @@
         <?php
         
 
-        function sort_2ways ($array,$order){
+        function sort_2way($array,$order){
             if ($order){
                 
                 echo "昇順にソートします。<br>";
@@ -29,10 +29,10 @@
         //配列を宣言
         $nums=[15,4,18,23,10];
 
-        sort_2ways($nums,true);
+        sort_2way($nums,true);
         
 
-        sort_2ways($nums,false);
+        sort_2way($nums,false);
         
         
        ?>
