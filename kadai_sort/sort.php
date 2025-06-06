15a02c1880fa19609952f65760ff254dc0299925
KAZSAND <kaz_410_nc@hotmail.com>
2025-06-07 06:42:29 +0900

    Add files via upload
    1stAttempt

---
+++ kadai_sort/sort.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP基礎編/Kadai_SORT</title>
+</head>
+<body>
+    <p>
+        <?php
+        
+
+        function sort_2ways ($array,$order){
+            if ($order==="ascending"){
+                
+                echo "昇順にソートします。<br>";
+                sort($array);
+
+            }else {
+                
+                echo "降順にソートします。<br>";
+                rsort($array);
+            }
+                
+            foreach ($array as $value){
+                echo $value . "<br>";
+            }
+        }
+        //配列を宣言
+        $nums=[15,4,18,23,10];
+
+        sort_2ways($nums,"ascending");
+        
+
+        sort_2ways($nums,false);
+        
+        
+       ?>
+
+    </p>
+
+    
+</body>
+</html>
